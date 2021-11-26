c5dc1badc3a1f870ae2e2a6a3f9822cda9f18784
Delyan Ninov <dninov@gmail.com>
2021-11-26 11:24:54 +0200

    new

--- index.js
+++ index.js
@@ -8,19 +8,23 @@ import postRoutes from './routes/posts.js'
 const app = express();
 dotenv.config();
 
-app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
-app.use(cors());
-app.use('/posts', postRoutes);
-app.get('/', (req, res)=>{
-    res.send('Welcome to the village API')
-});
 // Enable CORS
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
+});
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+app.get('/posts/:id', function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for all origins!'})
+  })
+
+app.use('/posts', postRoutes);
+app.get('/', (req, res)=>{
+    res.send('Welcome to the village API')
 });
 const PORT = process.env.PORT || 5000;
 
