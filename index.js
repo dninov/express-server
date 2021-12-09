6ce1dee31023280d6c7b0cddbe91ffc8013d4cad
Delyan Ninov <dninov@gmail.com>
2021-12-09 08:51:50 +0200

    make it better

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
+
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from  'dotenv';
-import postRoutes from './routes/posts.js'
+
+import postRoutes from './routes/posts.js';
+import userRouter from './routes/users.js';
 
 const app = express();
-dotenv.config();
 
-// Enable CORS
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-});
-app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
-app.get('/posts/:id', function (req, res, next) {
-    res.json({msg: 'This is CORS-enabled for all origins!'})
-  })
 
 app.use('/posts', postRoutes);
-app.get('/', (req, res)=>{
-    res.send('Welcome to the village API')
+app.use('/user', userRouter);
+
+app.get('/', (req, res) => {
+    res.send('APP IS RUNNING');
 });
+
+const CONNECTION_URL = 'mongodb+srv://dninov:criminal1@cluster0.ojfzc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(()=>app.listen(PORT, ()=> console.log(`Server is running on port: ${PORT}`)))
-    .catch((error)=> console.log(error.message));
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
 
 // mongoose.set('useFindAndModify', false);
