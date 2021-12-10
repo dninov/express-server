c6fa635b491db59208f099fcfdbe9f61adca2b88
Delyan Ninov <dninov@gmail.com>
2021-12-10 06:44:41 +0200

    remove env

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 import userRouter from './routes/users.js';
 
 const app = express();
+dotenv.config();
 
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
@@ -23,7 +24,7 @@ app.get('/', (req, res) => {
     }
 });
 
-const CONNECTION_URL = 'mongodb+srv://dninov:criminal1@cluster0.ojfzc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
